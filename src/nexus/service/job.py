9d37298e03c682b6c5f7d8d115c19d7d4bd52124
elyxlz <58893694+elyxlz@users.noreply.github.com>
2024-12-20 16:47:38 +0000

    more private git stuff (#18)
    * more private git stuff
    
    * lock

--- src/nexus/service/job.py
+++ src/nexus/service/job.py
@@ -85,8 +85,12 @@ def start_job(job: models.Job, gpu_index: int, jobs_dir: pathlib.Path, env_file:
 
     env["GIT_TERMINAL_PROMPT"] = "0"  # force git to fail if credentials dont exist
     try:
-        # Use git ls-remote to check repository access. This will fail if private and no creds.
-        subprocess.run(["git", "ls-remote", job.git_repo_url, "HEAD"], env=env, check=True, capture_output=True, text=True)
+        result = subprocess.run(["git", "ls-remote", job.git_repo_url, "HEAD"], env=env, capture_output=True, text=True)
+        # Check specifically for authentication errors in stderr
+        if "could not read Username" in result.stderr:
+            repo_accessible = False
+        else:
+            repo_accessible = result.returncode == 0 and not result.stderr
     except subprocess.CalledProcessError:
         repo_accessible = False
 
