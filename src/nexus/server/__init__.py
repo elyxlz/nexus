af816d527e1e3431df579f7c7d7bfc698ba214f3
elyx <elio@pascarelli.com>
2025-05-03 18:52:27 +0100

    wtf is that

--- src/nexus/server/__init__.py
+++
@@ -1,6 +0,0 @@
-from nexus.server.api.app import create_app
-from nexus.server.installation import setup
-
-# Expose FastAPI app directly for ASGI servers to import
-ctx = setup.initialize_context(setup.get_server_directory())
-app = create_app(ctx)
